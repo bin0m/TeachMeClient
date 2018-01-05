4e977706a03b84abe92f3f5bf25a0e1c366a0825
Gregory L <levitg@gmail.com>
2018-01-05 17:03:23 +0300

    Reworked Imaged Manager Blob Storage 1. Added download image and show images 2. Beautiful code :)

--- app/src/main/java/com/project/levitg/teachmeclient/BlobManagerActivity.java
+++ app/src/main/java/com/project/levitg/teachmeclient/BlobManagerActivity.java
@@ -1,123 +1,121 @@
 package com.project.levitg.teachmeclient;
 
+
+import android.app.AlertDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 
 public class BlobManagerActivity extends AppCompatActivity {
 
-    private int PICK_IMAGE_REQUEST = 1;
-    private Uri imageUri;
+    private static final int SELECT_IMAGE = 100;
 
-    public static final String storageConnectionString = "DefaultEndpointsProtocol=https;"
-            + "AccountName=teachme;"
-            + "AccountKey=00Fbb3fwzAZ0PRj5h3ilt+kWen/vosQuNQKauzau/UsIn92Ct+x5S/zCc8zsvPxM9HbSxeXmAbSdN6QNhMo2IA==";
+    private ImageView imageView;
+    private Uri imageUri;
+    private int imageLength;
+    private Button uploadImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blob_manager);
+
+        Button selectImageButton = (Button) findViewById(R.id.Select);
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SelectImageFromGallery();
+            }
+        });
+
+        this.uploadImageButton = (Button) findViewById(R.id.Upload);
+        this.uploadImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UploadImage();
+            }
+        });
+        this.uploadImageButton.setEnabled(false);
+
+        Button showImageButton = (Button) findViewById(R.id.Show);
+        showImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListImages();
+            }
+        });
+
+        this.imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    private void ListImages() {
+        Intent intent = new Intent(getBaseContext(), ListImagesActivity.class);
+        startActivity(intent);
+    }
+
+    private void SelectImageFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
+    }
+
+    private void UploadImage() {
+        try {
+            final InputStream imageStream = getContentResolver().openInputStream(this.imageUri);
+            final int imageLength = imageStream.available();
+
+            final Handler handler = new Handler();
+
+            Thread th = new Thread(new Runnable() {
+                public void run() {
+
+                    try {
+
+                        final String imageName = ImageManager.UploadImage(imageStream, imageLength);
+
+                        handler.post(new Runnable() {
+
+                            public void run() {
+                                Toast.makeText(BlobManagerActivity.this, "Image Uploaded Successfully. Name = " + imageName, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } catch (Exception ex) {
+                        final String exceptionMessage = ex.getMessage();
+                        handler.post(new Runnable() {
+                            public void run() {
+                                Toast.makeText(BlobManagerActivity.this, exceptionMessage, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            });
+            th.start();
+        } catch (Exception ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
 
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-
-            imageUri = data.getData();
-
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-
-                // Log.d(TAG, String.valueOf(bitmap));
-
-                ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                imageView.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        switch (requestCode) {
+            case SELECT_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    this.imageUri = imageReturnedIntent.getData();
+                    this.imageView.setImageURI(this.imageUri);
+                    this.uploadImageButton.setEnabled(true);
+                }
         }
-    }
-
-    public void ChooseImageClick(View view) {
-        Intent intent = new Intent();
-
-        // Show only images, no videos or anything else
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-
-        // Always show the chooser (if there are multiple options available)
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-    }
-
-    public void UploadResourceClick(View view) {
-        try {
-            final InputStream inputStream = getContentResolver().openInputStream(imageUri);
-            final int imageLength = inputStream.available();
-
-            new BlobUploadTask(this, (TextView) findViewById(R.id.textView), inputStream, imageLength, "testImg01.jpg")
-                    .execute();
-        } catch (final Throwable t) {
-            printException(t);
-        }
-
-    }
-
-    /**
-     * Prints the specified text value to the view and to LogCat.
-     *
-     * @param view  The view to print to.
-     * @param value The value to print.
-     */
-    public void outputText(final TextView view, final String value) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                view.append(value + "\n");
-                System.out.println(view);
-            }
-        });
-    }
-
-    /**
-     * Clears the text from the specified view.
-     *
-     * @param view The view to clear.
-     */
-    public void clearText(final TextView view) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                view.setText("");
-            }
-        });
-    }
-
-    /**
-     * Prints out the exception information .
-     */
-    public void printException(Throwable t) {
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter printWriter = new PrintWriter(stringWriter);
-        t.printStackTrace(printWriter);
-        outputText(
-                (TextView) findViewById(R.id.textView),
-                String.format(
-                        "Got an exception from running samples. Exception details:\n%s\n",
-                        stringWriter.toString()));
     }
 }
