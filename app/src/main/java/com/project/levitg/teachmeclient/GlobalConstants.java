f56f6fdff1c460fa6a792fc49d71bfcfc2fea42a
Gregory Levit <levitg@gmail.com>
2018-01-24 13:08:16 +0300

    Move to HTTPS

--- app/src/main/java/com/project/levitg/teachmeclient/GlobalConstants.java
+++ app/src/main/java/com/project/levitg/teachmeclient/GlobalConstants.java
@@ -7,7 +7,7 @@ package com.project.levitg.teachmeclient;
 public class GlobalConstants {
     //You need to change the IP if you testing environment is not local machine
     //or you may have different URL than we have here
-    public static final String BACKEND_URL = "http://teachmeserv.azurewebsites.net/";
+    public static final String BACKEND_URL = "https://teachmeserv.azurewebsites.net/";
     public static final String URL_SCHEME = "teachmeclient";
 }
 
